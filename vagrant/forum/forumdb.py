90d98d17ae068678670e304e31c781157cf37412
jhonnysanchezillisaca <jhonny15993@gmail.com>
2017-01-04 17:23:11 +0100

    Added .gitignore

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,13 +1,13 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
+import psycopg2
+import bleach
 
-## Database connection
-DB = []
 
-## Get posts from database.
+# Get posts from database.
 def GetAllPosts():
     '''Get all the posts from the database, sorted with the newest first.
 
@@ -16,16 +16,27 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in
+             cursor.fetchall())
+    DB.close()
     return posts
 
-## Add a post to the database.
+
+# Add a post to the database.
 def AddPost(content):
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    # Input sanitization of content
+    content = bleach.clean(content)
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("INSERT INTO posts (content) VALUES (%s)",
+                   (content,))
+    DB.commit()
+    DB.close()
